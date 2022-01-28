007fe356adde353160c91ddc9bb4d681f81fa21a
e33 <vitaliy.svinchyak@gmail.com>
2022-01-28 18:37:19 +0200

    concatenate rows

--- src/level/render/mod.rs
+++ src/level/render/mod.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
@@ -20,89 +21,115 @@ pub fn render_world(
     let map = read_level("debug");
 
     let now = Instant::now();
-    let (concatenated_voxels, not_used_voxels) = concatenate_voxels(&map);
+    let concatenated_voxels = concatenate_voxels(&map);
     let time = now.elapsed().as_millis();
     println!("concatenate_voxels time {}ms", time);
     println!("concatenations {}", concatenated_voxels.len());
-    println!("unused voxels {}", not_used_voxels.len());
     for (mesh_form, voxel) in concatenated_voxels {
         let pos = &voxel.position;
         let material = get_material(voxel.material, &materials);
         let mesh = meshes.add(mesh_form);
 
-        commands.spawn_bundle(PbrBundle {
+        let _ = commands.spawn_bundle(PbrBundle {
             mesh,
             material,
             transform: Transform::from_xyz(pos.x, pos.y, pos.z),
             ..Default::default()
         });
-    }
-
-    for voxel in not_used_voxels {
-        let pos = &voxel.position;
-        let material = get_material(voxel.material, &materials);
-        let mesh = get_entity_mesh(voxel.material, &meshes);
-
-        let mut entity_commands = commands.spawn_bundle(PbrBundle {
-            mesh,
-            material,
-            transform: Transform::from_xyz(pos.x, pos.y, pos.z),
-            ..Default::default()
-        });
-
-        if voxel.material == VoxelMaterial::OrangeLight {
-            spawn_orange_light_source(pos.x, pos.y, pos.z, &mut entity_commands);
-        }
-        if voxel.material == VoxelMaterial::BlueLight {
-            spawn_blue_light_source(pos.x, pos.y, pos.z, &mut entity_commands);
-        }
     }
 }
 
-fn concatenate_voxels(voxels: &Vec<Voxel>) -> (Vec<(Mesh, &Voxel)>, Vec<&Voxel>) {
-    let mut stats = HashMap::new();
-    let mut all_voxels = HashSet::new();
+#[derive(Debug)]
+struct XSequence<'a> {
+    start: &'a Voxel,
+    end: &'a Voxel,
+}
 
+fn concatenate_voxels(voxels: &Vec<Voxel>) -> Vec<(Mesh, &Voxel)> {
+    let mut stats = HashMap::new();
+
+    let mut min_y = usize::MAX;
+    let mut max_y = 0;
     for voxel in voxels {
         let z = voxel.position.z.round().to_string();
         if stats.get(&z).is_none() {
             stats.insert(z.clone(), vec![]);
         }
-        all_voxels.insert(voxel.id);
 
-        stats.get_mut(&z).unwrap().push(voxel);
+        let plane = stats.get_mut(&z).unwrap();
+        let y = voxel.position.y.round() as usize;
+        if plane.get(y).is_none() {
+            for _ in plane.len()..=y {
+                plane.push(vec![]);
+            }
+        }
+
+        plane[y].push(voxel);
+        max_y = cmp::max(y, max_y);
+        min_y = cmp::min(y, min_y);
     }
 
     let mut meshes = vec![];
 
-    for (_, row) in stats {
-        for voxel in &row {
-            let neighbours: Vec<&&Voxel> = row.iter().filter(|b| {
-                if !all_voxels.contains(&b.id) {
-                    return false;
-                }
-                if b.material != voxel.material {
-                    return false;
+    for (z, plate) in stats {
+        let z = z.parse::<usize>().unwrap();
+
+        for y in min_y..=max_y {
+            if let Some(row) = plate.get(y) {
+                if row.len() == 0 {
+                    continue;
                 }
 
-                let x_diff = (b.position.x as usize).abs_diff(voxel.position.x as usize);
-                let y_diff = (b.position.y as usize).abs_diff(voxel.position.y as usize);
+                let mut row = row.clone();
 
-                x_diff <= 1 && y_diff <= 1
-            }).collect();
+                row.sort_by(|a, b| {
+                    a.position.x.partial_cmp(&b.position.x).unwrap()
+                });
 
-            if neighbours.len() == 9 {
-                for neighbour in &neighbours {
-                    all_voxels.remove(&neighbour.id);
+                let mut x_sequences = vec![];
+                let mut start_voxel = row[0];
+                let mut prev_voxel = row[0];
+
+                for voxel in row.iter() {
+                    if voxel.id == prev_voxel.id {
+                        continue;
+                    }
+
+                    let is_end_of_sequence = voxel.position.x != prev_voxel.position.x + 1.0
+                        || voxel.material != prev_voxel.material;
+                    if is_end_of_sequence {
+                        x_sequences.push(
+                            XSequence {
+                                start: start_voxel,
+                                end: prev_voxel,
+                            });
+
+                        start_voxel = voxel;
+                    }
+
+                    prev_voxel = voxel;
                 }
-                meshes.push((Mesh::from(
-                    shape::Box::new(3.0, 3.0, 1.0)
-                ), *voxel));
+                x_sequences.push(
+                    XSequence {
+                        start: start_voxel,
+                        end: prev_voxel,
+                    });
+
+                for sequence in x_sequences {
+                    let shape = shape::Box {
+                        min_x: 0.0,
+                        max_x: sequence.end.position.x - sequence.start.position.x + 1.0,
+                        min_y: 0.0,
+                        max_y: sequence.end.position.y - sequence.start.position.y + 1.0,
+                        min_z: 0.0,
+                        max_z: sequence.end.position.z - sequence.start.position.z + 1.0,
+                    };
+
+                    meshes.push((Mesh::from(shape), sequence.start));
+                }
             }
         }
     }
 
-    let not_used_voxels: Vec<&Voxel> = voxels.iter().filter(|v| all_voxels.contains(&v.id)).collect();
-
-    (meshes, not_used_voxels)
+    meshes
 }
