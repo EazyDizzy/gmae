349456bc8f7d7bc1fa1f626dbdd606a35bb36ebb
Vitaliy-Svinchyak <vitaliy.svinchyak@gmail.com>
2022-04-23 09:45:41 +0300

    enable yaw rotation

--- src/player/system/camera.rs
+++ src/player/system/camera.rs
@@ -1,14 +1,17 @@
-use bevy::prelude::*;
+use bevy::input::mouse::MouseMotion;
 use bevy::math::vec3;
+use bevy::prelude::*;
 
 use crate::player::entity::Player;
 
 #[derive(Component)]
 pub struct PlayerCamera {
-    /// The current pitch of the FlyCamera in degrees. This value is always up-to-date, enforced by [FlyCameraPlugin](struct.FlyCameraPlugin.html)
+    /// The current pitch of the Camera in degrees.
     pub pitch: f32,
-    /// The current pitch of the FlyCamera in degrees. This value is always up-to-date, enforced by [FlyCameraPlugin](struct.FlyCameraPlugin.html)
+    /// The current pitch of the Camera in degrees.
     pub yaw: f32,
+    /// The sensitivity of the Camera's motion based on mouse movement. Defaults to `3.0`
+    pub sensitivity: f32,
 }
 
 impl Default for PlayerCamera {
@@ -16,7 +19,37 @@ impl Default for PlayerCamera {
         Self {
             pitch: 0.0,
             yaw: 0.0,
+            sensitivity: 3.0,
         }
+    }
+}
+
+fn mouse_motion_system(
+    time: Res<Time>,
+    mut mouse_motion_event_reader: EventReader<MouseMotion>,
+    mut query: Query<(&mut PlayerCamera, &mut Transform)>,
+) {
+    let mut delta: Vec2 = Vec2::ZERO;
+    for event in mouse_motion_event_reader.iter() {
+        delta += event.delta;
+    }
+    if delta.is_nan() {
+        return;
+    }
+
+    for (mut options, mut transform) in query.iter_mut() {
+        options.yaw -= delta.x * options.sensitivity * time.delta_seconds();
+        // options.pitch += delta.y * options.sensitivity * time.delta_seconds();
+
+        // options.pitch = options.pitch.clamp(-89.0, 89.9);
+        // println!("pitch: {}, yaw: {}", options.pitch, options.yaw);
+
+        let yaw_radians = options.yaw.to_radians();
+        let pitch_radians = options.pitch.to_radians();
+        dbg!(yaw_radians, pitch_radians);
+
+        transform.rotation = Quat::from_axis_angle(Vec3::Y, yaw_radians)
+            * Quat::from_axis_angle(-Vec3::X, pitch_radians);
     }
 }
 
@@ -43,7 +76,7 @@ fn setup_player_camera(mut commands: Commands) {
             ..Default::default()
         })
         .insert(PlayerCamera {
-            pitch: -45.0,
+            pitch: 0.0,
             yaw: 0.0,
             ..Default::default()
         });
@@ -55,6 +88,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_startup_system(setup_player_camera)
-            .add_system(camera_movement_system);
+            .add_system(camera_movement_system)
+            .add_system(mouse_motion_system);
     }
 }
